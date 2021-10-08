5ed596c161be7de51483243451542a7c73b13f6f
ramankala <rs.kala@hotmail.com>
2021-10-08 03:01:12 -0400

    Add function that prints out the books in array
    Made it print out books in array in order to figure out how to display.

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ function Book(title, author, numOfPages, haveRead){
 
 function addBookToLibrary(){
 
+    const book2 = new Book("bob", "dole", "200", "no");
+    const book3 = new Book("tea", "house", "300", "no");
+
+    myLibrary.push(book2);
+    myLibrary.push(book3);
+
     bookTitle = window.prompt("Enter the title of the book:");
     bookAuthor = window.prompt("Enter the author of the book:");
     bookPages = window.prompt("Enter the number of pages for the book:");
@@ -24,8 +30,15 @@ function addBookToLibrary(){
 
     const book1 = new Book(bookTitle, bookAuthor, bookPages, bookRead);
     myLibrary.push(book1);
+    displayBookFromLibrary();
     // console.log(book1.info());
 
 }
+
+function displayBookFromLibrary(){
+    myLibrary.forEach(function(item, index, array){
+        console.log(item);
+    })
+}
 addBookToLibrary();
-console.log(myLibrary);
+// console.log(myLibrary);
