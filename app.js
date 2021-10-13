8f3428fac1145b79500c838857d05c2c53ffb906
ramankala <rs.kala@hotmail.com>
2021-10-12 20:20:28 -0400

    Display each book in its own card on webpage)
    Wrote a function that loops through array and displays each book card.
    
    [Ticket: X]

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ let bookPages;
 let bookRead;
 
 const cardContainer = document.querySelector('#cardContainer');
-const dummyContainer1 = document.querySelector('#dummyContainer1');
-const dummy2Container = document.querySelector('#dummyContainer2');
+
+
+// bookTitle = window.prompt("Enter the title of the book:");
+// bookAuthor = window.prompt("Enter the author of the book:");
+// bookPages = window.prompt("Enter the number of pages for the book:");
+// bookRead = window.prompt("Have you read the book?:");
 
 function Book(title, author, numOfPages, haveRead){
     this.title = title
@@ -17,34 +21,40 @@ function Book(title, author, numOfPages, haveRead){
         console.log(title + " by " + author + ", " + numOfPages + ", " + haveRead)
     }
 }
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "Yes");
+const book2 = new Book("Game of Thrones", "George R.R. Martin", "694", "Yes");
+const book3 = new Book("The Name of the Wind", "Patrick Rothfuss", "662", "Yes");
+
+// function createBook(title, author, numOfPages, haveRead){
+//     const book = new Book(title, author, numOfPages, haveRead);
+// }
 
 function addBookToLibrary(){
 
-    const book2 = new Book("bob", "dole", "200", "no");
-    const book3 = new Book("tea", "house", "300", "no");
-
+    myLibrary.push(book1);
     myLibrary.push(book2);
     myLibrary.push(book3);
-
-    bookTitle = window.prompt("Enter the title of the book:");
-    bookAuthor = window.prompt("Enter the author of the book:");
-    bookPages = window.prompt("Enter the number of pages for the book:");
-    bookRead = window.prompt("Have you read the book?:");
-
-
-    const book1 = new Book(bookTitle, bookAuthor, bookPages, bookRead);
-    myLibrary.push(book1);
-    displayBookFromLibrary();
     // console.log(book1.info());
 
 }
 
 function displayBookFromLibrary(){
+    
+
+
     myLibrary.forEach(function(item, index, array){
+        let bookDiv = document.createElement("div");
+        bookDiv.setAttribute("id", "bookDiv");
         console.table(item);
-        cardContainer.textContent += item.title + " " + item.author + " " + item.numOfPages + " " + item.haveRead;
-    })
-    // cardContainer.textContent += myLibrary.join(",");
+        bookDiv.textContent += '"' + item.title + '"' + "\r\n" + item.author + "\r\nPages: " + item.numOfPages + "\r\nHave read?: " + item.haveRead + "\r\n";
+       
+        cardContainer.appendChild(bookDiv);
+    });
+    
 }
+
+
+// createBook(title, author,numOfPages, haveRead);
 addBookToLibrary();
+displayBookFromLibrary();
 // console.log(myLibrary);
