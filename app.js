173f56aabbef55b010bdf686c46bbee1700d9712
ramankala <rs.kala@hotmail.com>
2021-10-12 22:35:24 -0400

    Fix bug where books stacked during display
    The books were being stacked and reused resulting in duplicates, fixed
    bug.

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ let bookPages;
 let bookRead;
 
 const cardContainer = document.querySelector('#cardContainer');
+const addBookBtn = document.querySelector('#addBook');
 
-
-// bookTitle = window.prompt("Enter the title of the book:");
-// bookAuthor = window.prompt("Enter the author of the book:");
-// bookPages = window.prompt("Enter the number of pages for the book:");
-// bookRead = window.prompt("Have you read the book?:");
 
 function Book(title, author, numOfPages, haveRead){
     this.title = title
@@ -21,40 +17,58 @@ function Book(title, author, numOfPages, haveRead){
         console.log(title + " by " + author + ", " + numOfPages + ", " + haveRead)
     }
 }
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "Yes");
-const book2 = new Book("Game of Thrones", "George R.R. Martin", "694", "Yes");
-const book3 = new Book("The Name of the Wind", "Patrick Rothfuss", "662", "Yes");
+// const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295", "Yes");
+// const book2 = new Book("Game of Thrones", "George R.R. Martin", "694", "Yes");
+// const book3 = new Book("The Name of the Wind", "Patrick Rothfuss", "662", "Yes");
 
-// function createBook(title, author, numOfPages, haveRead){
-//     const book = new Book(title, author, numOfPages, haveRead);
-// }
+function createBook(title, author, numOfPages, haveRead){
 
-function addBookToLibrary(){
+    title = window.prompt("Enter the title of the book:");
+    author = window.prompt("Enter the author of the book:");
+    numOfPages = window.prompt("Enter the number of pages for the book:");
+    haveRead = window.prompt("Have you read the book?:");
 
-    myLibrary.push(book1);
-    myLibrary.push(book2);
-    myLibrary.push(book3);
+    const book = new Book(title, author, numOfPages, haveRead);
+
+    return book;
+}
+
+function addBookToLibrary(book){
+
+    myLibrary.push(book);
     // console.log(book1.info());
 
 }
 
 function displayBookFromLibrary(){
     
-
+    let bookDiv;
+    let removeBookBtn;
 
     myLibrary.forEach(function(item, index, array){
-        let bookDiv = document.createElement("div");
+        bookDiv = document.createElement("div");
         bookDiv.setAttribute("id", "bookDiv");
         console.table(item);
         bookDiv.textContent += '"' + item.title + '"' + "\r\n" + item.author + "\r\nPages: " + item.numOfPages + "\r\nHave read?: " + item.haveRead + "\r\n";
-       
-        cardContainer.appendChild(bookDiv);
+        removeBookBtn = document.createElement("button");
+        removeBookBtn.textContent = "Remove";
     });
+    cardContainer.appendChild(bookDiv);
+    cardContainer.appendChild(removeBookBtn);
     
 }
 
+addBookBtn.addEventListener('click', function(){
+
+    let book = createBook();
+    addBookToLibrary(book);
+    displayBookFromLibrary();
+});
+
+removeBookBtn.addEventListener('click', function(){
+    
+})
+
 
 // createBook(title, author,numOfPages, haveRead);
-addBookToLibrary();
-displayBookFromLibrary();
 // console.log(myLibrary);
