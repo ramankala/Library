ba73580a9f63128c38ce585aeb78bebcb5112e19
ramankala <rs.kala@hotmail.com>
2021-10-14 02:43:11 -0400

    Remove function for book successfully edits page.
    Remove button removes book from library array and html page.
    
    [Ticket: X]

--- app.js
+++ app.js
@@ -69,6 +69,9 @@ function displayBookFromLibrary(){
             pos = bookDiv.getAttribute("data-index-number");
             console.log(pos);
             removeBook(pos);
+            cardContainer.removeChild(bookDiv);
+            cardContainer.removeChild(removeBookBtn);
+            console.log(myLibrary);
             
         });
     });
