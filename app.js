6ebbc04a14b8540cdcd008375ceb175c7fa89c98
Raman <rs.kala@hotmail.com>
2021-12-09 04:14:16 -0500

    Refactor code to support classes
    Refactored the code so that it would function the same using classes.

--- app.js
+++ app.js
@@ -8,107 +8,112 @@ const cardContainer = document.querySelector('#cardContainer');
 const addBookBtn = document.querySelector('#addBook');
 
 
-function Book(title, author, numOfPages, haveRead){
+class Book{
+    
+    constructor(title, author, numOfPages, haveRead){
     this.title = title
     this.author = author
     this.numOfPages = numOfPages
     this.haveRead = haveRead
     this.info = function() {
         console.log(title + " by " + author + ", " + numOfPages + ", " + haveRead)
-    }
-}
-
-function createBook(title, author, numOfPages, haveRead){
-
-    title = window.prompt("Enter the title of the book:");
-    author = window.prompt("Enter the author of the book:");
-    numOfPages = window.prompt("Enter the number of pages for the book:");
-    haveRead = window.prompt("Have you read the book?(Y/N):");
-    haveRead = haveRead.toLowerCase();
-
-    if ((title == "" || title == null) || (author == "" || author == null) || (numOfPages == "" || numOfPages == null) || (haveRead == "" || haveRead == null)){
-
-    }
-    else{
-        const book = new Book(title, author, numOfPages, haveRead);
-
-        return book;
-
-    }
-}
-
-function addBookToLibrary(book){
-
-    myLibrary.push(book);
-
-}
-
-function removeBook(pos){
-    myLibrary.splice(pos, 1);
-    
-}
-
-function displayBookFromLibrary(){
-    
-    let bookDiv;
-    let removeBookBtn;
-
-    myLibrary.forEach(function(item, index, array){
-
-        bookDiv = document.createElement("div");
-        bookDiv.setAttribute("id", "bookDiv");
-        bookDiv.setAttribute("data-index-number", index);
-
-        bookDiv.textContent += '"' + item.title + '"' + "\r\n" + item.author + "\r\nPages: " + item.numOfPages;
-
-        removeBookBtn = document.createElement("button");
-        removeBookBtn.setAttribute("id", "removeBookBtn");
-        removeBookBtn.textContent = "Remove";
-
-        changeReadBtn = document.createElement("button");
-        changeReadBtn.setAttribute("id", "changeReadBtn");
-
-        if (item.haveRead == "y"){
-            item.haveRead = 'Read';
-            changeReadBtn.textContent = "Read";
-            changeReadBtn.classList.toggle('haveRead');
         }
-        else {
-            item.haveRead = 'Not Read';
-            changeReadBtn.textContent = "Not Read";
-            changeReadBtn.classList.toggle('notRead');
+    }
+
+    createBook(title, author, numOfPages, haveRead){
+
+        title = window.prompt("Enter the title of the book:");
+        author = window.prompt("Enter the author of the book:");
+        numOfPages = window.prompt("Enter the number of pages for the book:");
+        haveRead = window.prompt("Have you read the book?(Y/N):");
+        haveRead = haveRead.toLowerCase();
+
+        if ((title == "" || title == null) || (author == "" || author == null) || (numOfPages == "" || numOfPages == null) || (haveRead == "" || haveRead == null)){
+
         }
+        else{
+            const book = new Book(title, author, numOfPages, haveRead);
 
-        removeBookBtn.addEventListener('click', function(){
-            let pos;
+            return book;
 
-            pos = bookDiv.getAttribute("data-index-number");
-            removeBook(pos);
-            cardContainer.removeChild(bookDiv);
-            cardContainer.removeChild(removeBookBtn);
-            
-        });
+        }
+    }
 
-        changeReadBtn.addEventListener('click', function(e){
+    addBookToLibrary(book){
 
+        myLibrary.push(book);
 
-            if (e.target.className == 'haveRead') {
-                item.haveRead = "Not Read";
-                e.target.textContent = 'Not Read';
-                e.target.classList.toggle('haveRead');
-                e.target.classList.toggle('notRead');
+    }
 
+    removeBook(pos){
+        myLibrary.splice(pos, 1);
+    
+    }
+
+    displayBookFromLibrary(){
+    
+        let bookDiv;
+        let removeBookBtn;
+        let changeReadBtn;
+        let buttonContainer;
+
+        myLibrary.forEach(function(item, index, array){
+
+            bookDiv = document.createElement("div");
+            bookDiv.setAttribute("id", "bookDiv");
+            bookDiv.setAttribute("data-index-number", index);
+
+            bookDiv.textContent += '"' + item.title + '"' + "\r\n" + item.author + "\r\nPages: " + item.numOfPages;
+
+            removeBookBtn = document.createElement("button");
+            removeBookBtn.setAttribute("id", "removeBookBtn");
+            removeBookBtn.textContent = "Remove";
+
+            changeReadBtn = document.createElement("button");
+            changeReadBtn.setAttribute("id", "changeReadBtn");
+
+            if (item.haveRead == "y"){
+                item.haveRead = 'Read';
+                changeReadBtn.textContent = "Read";
+                changeReadBtn.classList.toggle('haveRead');
             }
-            else if (e.target.className == 'notRead'){
-                item.haveRead = "Read";
-                e.target.textContent = 'Read';
-                e.target.classList.toggle('notRead');
-                e.target.classList.toggle('haveRead');
-
+            else {
+                item.haveRead = 'Not Read';
+                changeReadBtn.textContent = "Not Read";
+                changeReadBtn.classList.toggle('notRead');
             }
 
+            removeBookBtn.addEventListener('click', function(){
+                let pos;
+                let book = new Book();
+
+                pos = bookDiv.getAttribute("data-index-number");
+                book.removeBook(pos);
+                cardContainer.removeChild(bookDiv);
+                
+            });
+
+            changeReadBtn.addEventListener('click', function(e){
+
+
+                if (e.target.className == 'haveRead') {
+                    item.haveRead = "Not Read";
+                    e.target.textContent = 'Not Read';
+                    e.target.classList.toggle('haveRead');
+                    e.target.classList.toggle('notRead');
+
+                }
+                else if (e.target.className == 'notRead'){
+                    item.haveRead = "Read";
+                    e.target.textContent = 'Read';
+                    e.target.classList.toggle('notRead');
+                    e.target.classList.toggle('haveRead');
+
+                }
+
+            });
         });
-    });
+
     buttonContainer = document.createElement('div');
     buttonContainer.setAttribute("id", "buttonContainer");
     buttonContainer.appendChild(changeReadBtn);
@@ -117,12 +122,22 @@ function displayBookFromLibrary(){
     cardContainer.appendChild(bookDiv);
     
     
+    }
+
+    createLibrary(){
+
+
+    }
+
 }
 
 
 addBookBtn.addEventListener('click', function(){
 
-    let book = createBook();
-    addBookToLibrary(book);
-    displayBookFromLibrary();
+    let book = new Book();
+    let newEntry;
+
+    newEntry = book.createBook();
+    book.addBookToLibrary(newEntry);
+    book.displayBookFromLibrary();
 });
